3d08353fee79aee9d077bd5e5811d0dde15db969
Gary Locke <garylocke@Garys-MacBook-Pro.local>
2014-06-21 08:05:39 -0400

    Fixed minor bugs.

--- app/views/layout/main/main.blade.php
+++ app/views/layout/main/main.blade.php
@@ -7,9 +7,18 @@
             <label ng-show="!loadingSub">Enter a subreddit or multireddit (e.g. "pics+funny"):</label>
             <label ng-show="loadingSub">Loading...</label><br />
             <div class="input-group">
-                <input class="form-control" id="sub" type="text" ng-model="sub" placeholder="loading..." ng-disabled="loadingSub" />
+                <input class="form-control"
+                       id="sub"
+                       type="text"
+                       ng-model="sub"
+                       placeholder="loading..."
+                       ng-disabled="loadingSub" />
                 <span class="input-group-btn">
-                    <button class="btn btn-default" type="submit" ng-disabled="loadingSub||sub==''||sub==undefined">submit</button>
+                    <button class="btn btn-default"
+                            type="submit"
+                            ng-disabled="loadingSub||sub==''||sub==undefined">
+                        submit
+                    </button>
                 </span>
             </div>
         </form>
@@ -19,30 +28,58 @@
 <div id="brick-wall" masonry>
     <div class="brick masonry-brick" ng-repeat="post in posts track by $index">
         <div class="title">
-            <a class="subLink" href="#" ng-click="getSub(post.data.subreddit)">r/@{{ post.data.subreddit }}</a>
+            <a class="subLink" href="#"
+               ng-click="getSub(post.data.subreddit)">
+                r/@{{ post.data.subreddit }}
+            </a>
             -
-            <a class="titleLink" ng-href="@{{ post.data.url }}" target="_blank">@{{ post.data.title }}</a>
+            <a class="titleLink"
+               ng-href="@{{ post.data.url }}"
+               target="_blank">
+                @{{ post.data.title }}
+            </a>
         </div>
         <div class="content-outer">
-            <div class="content-inner" brick-content post-data="post.data"></div>
+            <div class="content-inner"
+                 brick-content post-data="post.data">
+            </div>
         </div>
         <div class="info">
             <div class="left">
-                <strong><a href="http://reddit.com/u/@{{ post.data.author }}" tooltip="View profile" target="_blank" >@{{ post.data.author }}</a></strong><br/>
+                <strong>
+                    <a href="http://reddit.com/u/@{{ post.data.author }}"
+                           tooltip="View profile"
+                           target="_blank">
+                        @{{ post.data.author }}
+                    </a>
+                </strong><br/>
                 Posted: @{{ getPostAge(post.data.created) }}<br/>
                 Comments:
-                <a href="http://www.reddit.com@{{ post.data.permalink }}" tooltip="View comments" target="_blank">
+                <a href="http://www.reddit.com@{{ post.data.permalink }}"
+                   tooltip="View comments"
+                   target="_blank">
                     @{{ post.data.num_comments }}
                 </a>
             </div>
             <div class="right">
                 <div ng-show="loggedIn">
                     @{{ post.data.ups }}
-                    <i id="@{{ post.data.name }}_up" ng-class="{up: loggedIn && post.data.likes && post.data.likes != null}" ng-click="submitVote(post.data.name,post.data.likes,1)" class="fa fa-arrow-up"></i><br />
+                    <i id="@{{ post.data.name }}_up"
+                       ng-class="{up: loggedIn && post.data.likes && post.data.likes != null}"
+                       ng-click="submitVote(post.data.name,post.data.likes,1)"
+                       class="fa fa-arrow-up">
+                    </i><br />
                     @{{ post.data.downs }}
-                    <i ng-show="loggedIn" id="@{{ post.data.name }}_down" ng-class="{down: loggedIn && !post.data.likes && post.data.likes != null}" ng-click="submitVote(post.data.name,post.data.likes,-1)" class="fa fa-arrow-down"></i>
+                    <i ng-show="loggedIn"
+                       id="@{{ post.data.name }}_down"
+                       ng-class="{down: loggedIn && !post.data.likes && post.data.likes != null}"
+                       ng-click="submitVote(post.data.name,post.data.likes,-1)"
+                       class="fa fa-arrow-down">
+                   </i>
                 </div>
-                <div ng-show="!loggedIn" tooltip="Log in first!" ng-click="authorizeAccount()">
+                <div ng-show="!loggedIn"
+                     tooltip="Log in first!"
+                     ng-click="authorizeAccount()">
                     @{{ post.data.ups }}
                     <i class="fa fa-arrow-up"></i><br />
                     @{{ post.data.downs }}
