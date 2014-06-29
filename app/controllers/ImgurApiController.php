d6ac71fab0d0e1b0b38b0a2ccbadc78caf95d3ec
Gary Locke <garylocke@Garys-MacBook-Pro.local>
2014-06-29 10:28:32 -0400

    Cleaned up ImgurApiController logic a bit.

--- app/controllers/ImgurApiController.php
+++ app/controllers/ImgurApiController.php
@@ -31,108 +31,109 @@ class ImgurApiController extends BaseController{
 
         // Check Imgur cache in Mongo before calling API for data.
         $cache_results = $this->imgurCacheModel->getCacheStore($id,$type);
-        if(array_key_exists(0,$cache_results))
-            if(array_key_exists('images',$cache_results[0]))
-                if(count($cache_results[0]['images']) >= 1)
-                {
-                    Log::info('Imgur result',array('id' => $id, 'status' => 'GOOD', 'from' => 'cache'));
-                    $response = array(
-                        "status" => 'GOOD',
-                        "from" => 'cache',
-                        "images" => $cache_results[0]['images']
-                    );
-                    return Response::json($response);
-                }
-
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Authorization: Client-ID ' . $this->client['id']
-        ));
-
-        $results = json_decode(curl_exec($ch));
-        curl_close($ch);
-
-        $images = array();
-        if(is_object($results))
+        if(array_key_exists(0,$cache_results) && array_key_exists('images',$cache_results[0]) && count($cache_results[0]['images']) >= 1)
         {
-            if(property_exists($results,'success') && $results->success)
+            Log::info('Imgur result',array('id' => $id, 'status' => 'GOOD', 'from' => 'cache'));
+            $response = array(
+                "status" => 'GOOD',
+                "from" => 'cache',
+                "images" => $cache_results[0]['images']
+            );
+            return Response::json($response);
+        }
+        else
+        {
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Authorization: Client-ID ' . $this->client['id']
+            ));
+
+            $results = json_decode(curl_exec($ch));
+            curl_close($ch);
+
+            $images = array();
+            if(is_object($results))
             {
-                if(property_exists($results,'data'))
+                if(property_exists($results,'success') && $results->success)
                 {
-                    if(is_object($results->data))
+                    if(property_exists($results,'data'))
                     {
-                        if(property_exists($results->data,'is_album') && $results->data->is_album)
+                        if(is_object($results->data))
                         {
-                            if(property_exists($results->data,'images'))
+                            if(property_exists($results->data,'is_album') && $results->data->is_album)
                             {
-                                foreach($results->data->images as $image)
+                                if(property_exists($results->data,'images'))
                                 {
-                                    if(property_exists($image,'id') && property_exists($image,'title') && property_exists($image,'description') && property_exists($image,'link'))
+                                    foreach($results->data->images as $image)
                                     {
-                                        array_push(
-                                            $images,
-                                            array(
-                                                'id' => $image->id,
-                                                'title' => $image->title,
-                                                'description' => $image->description,
-                                                'link' => $image->link
-                                            )
-                                        );
+                                        if(property_exists($image,'id') && property_exists($image,'title') && property_exists($image,'description') && property_exists($image,'link'))
+                                        {
+                                            array_push(
+                                                $images,
+                                                array(
+                                                    'id' => $image->id,
+                                                    'title' => $image->title,
+                                                    'description' => $image->description,
+                                                    'link' => $image->link
+                                                )
+                                            );
+                                        }
                                     }
                                 }
                             }
-                        }
-                        else if(property_exists($results->data,'id') && property_exists($results->data,'title') && property_exists($results->data,'description') && property_exists($results->data,'link'))
-                        {
-                            array_push(
-                                $images,
-                                array(
-                                    'id' => $results->data->id,
-                                    'title' => $results->data->title,
-                                    'description' => $results->data->description,
-                                    'link' => $results->data->link
-                                )
-                            );
-                        }
-                    }
-                    else if(is_array($results->data))
-                    {
-                        foreach($results->data as $image)
-                        {
-                            if(property_exists($image,'id') && property_exists($image,'title') && property_exists($image,'description') && property_exists($image,'link'))
+                            else if(property_exists($results->data,'id') && property_exists($results->data,'title') && property_exists($results->data,'description') && property_exists($results->data,'link'))
                             {
                                 array_push(
                                     $images,
                                     array(
-                                        'id' => $image->id,
-                                        'title' => $image->title,
-                                        'description' => $image->description,
-                                        'link' => $image->link
+                                        'id' => $results->data->id,
+                                        'title' => $results->data->title,
+                                        'description' => $results->data->description,
+                                        'link' => $results->data->link
                                     )
                                 );
+                            }
+                        }
+                        else if(is_array($results->data))
+                        {
+                            foreach($results->data as $image)
+                            {
+                                if(property_exists($image,'id') && property_exists($image,'title') && property_exists($image,'description') && property_exists($image,'link'))
+                                {
+                                    array_push(
+                                        $images,
+                                        array(
+                                            'id' => $image->id,
+                                            'title' => $image->title,
+                                            'description' => $image->description,
+                                            'link' => $image->link
+                                        )
+                                    );
+                                }
                             }
                         }
                     }
                 }
             }
+
+            // Create cache store for this result.
+            if(!$this->imgurCacheModel->createCacheStore($id, $type, $images))
+                Log::error('Error while storing image cache for ID ' . $id);
+
+            if(count($images) >= 1){
+                Log::info('Imgur result',array('id' => $id, 'status' => 'GOOD', 'from' => 'api'));
+                $response = array(
+                    "status" => 'GOOD',
+                    "from" => 'api',
+                    "images" => $images
+                );
+            } else $response = array("status" => 'FAIL');
+
+            return Response::json($response);
         }
 
-        // Create cache store for this result.
-        if(!$this->imgurCacheModel->createCacheStore($id, $type, $images))
-            Log::error('Error while storing image cache for ID ' . $id);
-
-        if(count($images) >= 1){
-            Log::info('Imgur result',array('id' => $id, 'status' => 'GOOD', 'from' => 'api'));
-            $response = array(
-                "status" => 'GOOD',
-                "from" => 'api',
-                "images" => $images
-            );
-        } else $response = array("status" => 'FAIL');
-
-        return Response::json($response);
     }
 }
